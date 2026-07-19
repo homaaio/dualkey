7e4a4014a267e0df71e9db7b876df26e18c58192
homa <krutyehomaibusa@gmail.com>
2026-07-19 16:39:18 +0500

    changed main.py for new UI

--- src/linux/main.py
+++ src/linux/main.py
@@ -1,3 +1,4 @@
+# src/linux/main.py
 #!/usr/bin/env python3
 import sys
 import os
@@ -7,6 +8,7 @@ from joystick_reader import JoystickReader
 from keyboard_emulator import KeyboardEmulator
 from joystick_hider import JoystickHider
 from web_server import start_web_server
+from gui import ReJoyGUI
 
 DEFAULT_BINDINGS = {
     'left_stick_up': 'w',
@@ -36,14 +38,13 @@ class ReJoyLinux:
     def __init__(self):
         self.bindings = DEFAULT_BINDINGS.copy()
         self.emulation_enabled = False
-        self.hidden = False
         self.deadzone = 0.3
         self.running = True
         
         self.reader = JoystickReader()
         self.emulator = KeyboardEmulator()
         self.hider = JoystickHider()
-        
+    
     def get_state(self):
         return {
             'connected': self.reader.connected,
@@ -61,39 +62,43 @@ class ReJoyLinux:
         
         dz = self.deadzone
         
-        # Left stick
         lx = self.reader.axes.get('left_x', 0)
         ly = self.reader.axes.get('left_y', 0)
+        rx = self.reader.axes.get('right_x', 0)
+        ry = self.reader.axes.get('right_y', 0)
         
-        if lx < -dz: self.emulator.press('left_stick_left', self.bindings)
-        elif lx > dz: self.emulator.press('left_stick_right', self.bindings)
+        if lx < -dz:
+            self.emulator.press('left_stick_left', self.bindings)
+        elif lx > dz:
+            self.emulator.press('left_stick_right', self.bindings)
         else:
             self.emulator.release('left_stick_left', self.bindings)
             self.emulator.release('left_stick_right', self.bindings)
         
-        if ly < -dz: self.emulator.press('left_stick_up', self.bindings)
-        elif ly > dz: self.emulator.press('left_stick_down', self.bindings)
+        if ly < -dz:
+            self.emulator.press('left_stick_up', self.bindings)
+        elif ly > dz:
+            self.emulator.press('left_stick_down', self.bindings)
         else:
             self.emulator.release('left_stick_up', self.bindings)
             self.emulator.release('left_stick_down', self.bindings)
         
-        # Right stick
-        rx = self.reader.axes.get('right_x', 0)
-        ry = self.reader.axes.get('right_y', 0)
-        
-        if rx < -dz: self.emulator.press('right_stick_left', self.bindings)
-        elif rx > dz: self.emulator.press('right_stick_right', self.bindings)
+        if rx < -dz:
+            self.emulator.press('right_stick_left', self.bindings)
+        elif rx > dz:
+            self.emulator.press('right_stick_right', self.bindings)
         else:
             self.emulator.release('right_stick_left', self.bindings)
             self.emulator.release('right_stick_right', self.bindings)
         
-        if ry < -dz: self.emulator.press('right_stick_up', self.bindings)
-        elif ry > dz: self.emulator.press('right_stick_down', self.bindings)
+        if ry < -dz:
+            self.emulator.press('right_stick_up', self.bindings)
+        elif ry > dz:
+            self.emulator.press('right_stick_down', self.bindings)
         else:
             self.emulator.release('right_stick_up', self.bindings)
             self.emulator.release('right_stick_down', self.bindings)
         
-        # Buttons
         button_map = {
             'cross': 'cross', 'circle': 'circle', 'triangle': 'triangle',
             'square': 'square', 'l1': 'l1', 'r1': 'r1', 'l2': 'l2',
@@ -106,36 +111,30 @@ class ReJoyLinux:
                 self.emulator.press(action, self.bindings)
             else:
                 self.emulator.release(action, self.bindings)
-        
-        # D-pad
-        dpad = self.reader.buttons.get('dpad', (0, 0))
-        if dpad[0] == -1: self.emulator.press('dpad_left', self.bindings)
-        elif dpad[0] == 1: self.emulator.press('dpad_right', self.bindings)
-        else:
-            self.emulator.release('dpad_left', self.bindings)
-            self.emulator.release('dpad_right', self.bindings)
-        
-        if dpad[1] == 1: self.emulator.press('dpad_up', self.bindings)
-        elif dpad[1] == -1: self.emulator.press('dpad_down', self.bindings)
-        else:
-            self.emulator.release('dpad_up', self.bindings)
-            self.emulator.release('dpad_down', self.bindings)
     
     def run(self):
-        print('ReJoy Linux starting...')
-        print('Web interface: http://localhost:8080')
+        print("ReJoy Linux starting...")
+        print("GUI opened in separate window")
+        print("Web interface: http://localhost:8080")
         
-        web_thread = threading.Thread(target=start_web_server, args=(self,))
-        web_thread.daemon = True
+        web_thread = threading.Thread(target=start_web_server, args=(self,), daemon=True)
         web_thread.start()
         
-        try:
+        gui = ReJoyGUI(self)
+        
+        def update_loop():
             while self.running:
                 self.reader.update()
                 self.process_emulation()
                 time.sleep(0.016)
+        
+        update_thread = threading.Thread(target=update_loop, daemon=True)
+        update_thread.start()
+        
+        try:
+            gui.run()
         except KeyboardInterrupt:
-            print('\nShutting down...')
+            print("\nShutting down...")
         finally:
             self.emulator.release_all(self.bindings)
             self.running = False
