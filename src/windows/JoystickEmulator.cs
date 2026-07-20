e2a5171b19bb4dd92cfbd505b1a44158cb5342b1
homaaio <krutyehomaibusa@gmail.com>
2026-07-20 11:06:32 +0500

    updated UI (fixed)

--- src/windows/JoystickEmulator.cs
+++ src/windows/JoystickEmulator.cs
@@ -1,3 +1,4 @@
+// src/windows/JoystickEmulator.cs
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -15,46 +16,69 @@ namespace DualKey
         public bool Enabled { get; set; } = false;
         public float Deadzone { get; set; } = 0.3f;
 
-        public Dictionary<string, int> Bindings { get; private set; }
+        public Dictionary<string, int> Bindings { get; set; }
         private HashSet<int> pressedKeys = new HashSet<int>();
 
         public JoystickEmulator()
         {
             Bindings = new Dictionary<string, int>
             {
-                // Левый стик - WASD
-                {"left_stick_up", 0x57},      // W
-                {"left_stick_down", 0x53},    // S
-                {"left_stick_left", 0x41},    // A
-                {"left_stick_right", 0x44},   // D
-                
-                // Правый стик - стрелки
-                {"right_stick_up", 0x26},     // Up arrow
-                {"right_stick_down", 0x28},   // Down arrow
-                {"right_stick_left", 0x25},   // Left arrow
-                {"right_stick_right", 0x27},  // Right arrow
-                
-                // Кнопки действий
-                {"cross", 0x20},              // Space
-                {"circle", 0x45},             // E
-                {"triangle", 0x51},           // Q
-                {"square", 0x52},             // R
-                
-                // Бамперы и триггеры
-                {"l1", 0x10},                 // Shift
-                {"r1", 0x11},                 // Ctrl
-                {"l2", 0x31},                 // 1
-                {"r2", 0x32},                 // 2
-                
-                // Стики (нажатия)
-                {"l3", 0x46},                 // F
-                {"r3", 0x47},                 // G
-                
-                // Системные
-                {"select", 0x09},             // Tab
-                {"start", 0x0D},              // Enter
-                {"ps_button", 0x1B}           // Escape
+                {"left_stick_up", 0x57},
+                {"left_stick_down", 0x53},
+                {"left_stick_left", 0x41},
+                {"left_stick_right", 0x44},
+                {"right_stick_up", 0x26},
+                {"right_stick_down", 0x28},
+                {"right_stick_left", 0x25},
+                {"right_stick_right", 0x27},
+                {"cross", 0x20},
+                {"circle", 0x45},
+                {"triangle", 0x51},
+                {"square", 0x52},
+                {"l1", 0x10},
+                {"r1", 0x11},
+                {"l2", 0x31},
+                {"r2", 0x32},
+                {"l3", 0x46},
+                {"r3", 0x47},
+                {"select", 0x09},
+                {"start", 0x0D},
+                {"ps_button", 0x1B},
             };
+        }
+
+        public void ResetBindings()
+        {
+            Bindings = new Dictionary<string, int>
+            {
+                {"left_stick_up", 0x57},
+                {"left_stick_down", 0x53},
+                {"left_stick_left", 0x41},
+                {"left_stick_right", 0x44},
+                {"right_stick_up", 0x26},
+                {"right_stick_down", 0x28},
+                {"right_stick_left", 0x25},
+                {"right_stick_right", 0x27},
+                {"cross", 0x20},
+                {"circle", 0x45},
+                {"triangle", 0x51},
+                {"square", 0x52},
+                {"l1", 0x10},
+                {"r1", 0x11},
+                {"l2", 0x31},
+                {"r2", 0x32},
+                {"l3", 0x46},
+                {"r3", 0x47},
+                {"select", 0x09},
+                {"start", 0x0D},
+                {"ps_button", 0x1B},
+            };
+        }
+
+        public void UpdateBinding(string action, int keyCode)
+        {
+            if (Bindings.ContainsKey(action))
+                Bindings[action] = keyCode;
         }
 
         public void PressKey(string action)
